d84210aa5ca8fda021c7fd0d3edf3560335fe8a4
OdincovMD <hardbox.top@yandex.ru>
2025-05-29 22:39:02 +0300

    Добавление лабораторной 5

---
+++ lab05/users_items/build.sbt
@@ -0,0 +1,10 @@
+version := "1.0"
+
+scalaVersion := "2.12.18"
+
+name := "users_items"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.4.3" % "provided",
+  "org.apache.spark" %% "spark-sql"  % "3.4.3" % "provided"
+)
---
+++ lab05/users_items/src/main/scala/users_items.scala
@@ -0,0 +1,85 @@
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import org.apache.spark.sql.functions._
+import java.time.format.DateTimeFormatter
+import java.time.LocalDate
+import org.apache.hadoop.fs.{FileSystem, Path}
+
+object users_items {
+
+  def normalize(name: String): String =
+    name.toLowerCase.replaceAll("[\\s\\-]+", "_")
+
+  def readEvents(spark: SparkSession, path: String, prefix: String): DataFrame = {
+    import spark.implicits._
+    spark.read.parquet(path)
+      .select($"uid", $"item_id", $"date")
+      .withColumn("item_id", concat(lit(prefix + "_"), regexp_replace(lower($"item_id"), "[\\s\\-]+", "_")))
+  }
+
+  def pivot(df: DataFrame): DataFrame = {
+    df.groupBy("uid")
+      .pivot("item_id")
+      .agg(count("*"))
+      .na.fill(0)
+  }
+
+  def latestDate(df: DataFrame): String = {
+    val maxDate = df.agg(max(to_date($"date", "yyyyMMdd"))).as[java.sql.Date].head()
+    val localDate = maxDate.toLocalDate
+    localDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+  }
+
+  def latestOutputSubdir(spark: SparkSession, outputDir: String): Option[String] = {
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    val base = new Path(outputDir)
+    if (!fs.exists(base)) return None
+
+    val subdirs = fs.listStatus(base).filter(_.isDirectory).map(_.getPath.getName)
+    subdirs.filter(_.matches("\\d{8}")).sorted.lastOption
+  }
+
+  def mergeMatrices(df1: DataFrame, df2: DataFrame): DataFrame = {
+    val allCols = (df1.columns ++ df2.columns).distinct
+    val filled1 = allCols.foldLeft(df1)((df, col) => if (df.columns.contains(col)) df else df.withColumn(col, lit(0)))
+    val filled2 = allCols.foldLeft(df2)((df, col) => if (df.columns.contains(col)) df else df.withColumn(col, lit(0)))
+
+    filled1.unionByName(filled2)
+      .groupBy("uid")
+      .agg(
+        allCols.filter(_ != "uid").map(c => sum(col(c)).alias(c)): _*
+      )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.implicits._
+
+    val updateMode = spark.conf.get("spark.users_items.update", "1").toInt
+    val inputDir = spark.conf.get("spark.users_items.input_dir")
+    val outputDir = spark.conf.get("spark.users_items.output_dir")
+
+    val viewDF = readEvents(spark, s"$inputDir/view", "view")
+    val buyDF = readEvents(spark, s"$inputDir/buy", "buy")
+    val allDF = viewDF.union(buyDF)
+
+    val inputPivoted = pivot(allDF)
+    val dateStr = latestDate(allDF)
+    val finalOutputPath = s"$outputDir/$dateStr"
+
+    val resultDF = if (updateMode == 0) {
+      inputPivoted
+    } else {
+      val previousDirOpt = latestOutputSubdir(spark, outputDir)
+      previousDirOpt match {
+        case Some(prevDate) =>
+          val prevDF = spark.read.option("mergeSchema", "true").parquet(s"$outputDir/$prevDate")
+          mergeMatrices(prevDF, inputPivoted)
+        case None =>
+          inputPivoted
+      }
+    }
+
+    resultDF.write.mode("overwrite").parquet(finalOutputPath)
+    println(s"Матрица users-items записана в $finalOutputPath")
+  }
+}
